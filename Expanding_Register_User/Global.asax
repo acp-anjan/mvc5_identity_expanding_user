5aea57edc52ec7a5dbcbecba85e102de22977d92
acp-anjan <paudelanjanchandra@gmail.com>
2020-07-05 22:59:14 +0545

    @anjan first commit

---
+++ Expanding_Register_User/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Expanding_Register_User.MvcApplication" Language="C#" %>
---
+++ Expanding_Register_User/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Expanding_Register_User.Startup))]
+namespace Expanding_Register_User
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
